02f194880427f0386e1bbfe3cae1a6edd135465b
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-04-18 15:31:30 +0900

    20

--- UiPickerView/ViewController.swift
+++ UiPickerView/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     
     @IBOutlet weak var myPickerView: UIPickerView!
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var yourLabel: UILabel!
+    
+    var color = ["WHITE","GREEN","RED"]
+    var feeling = ["1","2","3","4"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         myPickerView.delegate=self
         myPickerView.dataSource=self
+        myLabel.text=color[0]
+        yourLabel.text=feeling[0]
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -26,19 +33,28 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if component == 0{
-            return 10
+            return 3
         }
         else {
-            return 100
+            return 4
         }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        let myString = "\(row), \(component)"
-        myLabel.text=myString
-        return myString
+        if component == 0{
+            return color[row]
+        }
+        else{
+            return feeling[row]
+        }
     }
 
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            myLabel.text = color[row]
+        } else {
+            yourLabel.text = feeling[row]
+        }
+    }
 }
 
